9e0e29def9953b7ddf19f1011fc6469dcd6837fe
Sean Engelstad <sean.engelstad1@gmail.com>
2025-09-12 00:03:10 -0400

    update pivot dense LU

--- examples/0_demos/_utils.py
+++ examples/0_demos/_utils.py
@@ -1,10 +1,11 @@
 import numpy as np
 from qordering import BeamFem, PlateAssembler
+import scipy as sp
 
-def get_beam_csr_mat_and_rhs(nxe=300, csr:bool=True):
+def get_beam_csr_mat_and_rhs(nxe=300, csr:bool=True, remove_bcs:bool=False):
     E = 2e7; b = 4e-3; L = 1; rho = 1
     qmag, ys, rho_KS = 2e-2, 4e5, 50.0
-    nxh = np.min([100, nxe])
+    nxh = nxe
     hvec = np.array([1e-2] * nxh)
 
     # create and assemble FEA problem
@@ -14,9 +15,16 @@ def get_beam_csr_mat_and_rhs(nxe=300, csr:bool=True):
     mat, rhs = beam_fea.Kmat, beam_fea.force
     if not csr:
         mat = mat.toarray()
+
+    if remove_bcs:
+        mat = mat.toarray()
+        mat, rhs = remove_bcs(mat, rhs)
+        if csr:
+            mat = sp.sparse.linalg.csr_matrix(mat)
+
     return mat, rhs
 
-def get_plate_csr_mat_and_rhs(nxe=300, csr:bool=True):
+def get_plate_csr_mat_and_rhs(nxe=300, csr:bool=True, remove_bcs:bool=False):
     nxc = 1
     plate_fea = PlateAssembler.aluminum_unitsquare_trigload(
         num_elements=nxe**2,
@@ -34,5 +42,25 @@ def get_plate_csr_mat_and_rhs(nxe=300, csr:bool=True):
     mat, rhs = plate_fea.Kmat, plate_fea.force
     if not csr:
         mat = mat.toarray()
+
+    if remove_bcs:
+        mat = mat.toarray()
+        mat, rhs = remove_bcs(mat, rhs)
+        if csr:
+            mat = sp.sparse.linalg.csr_matrix(mat)
+
     return mat, rhs
 
+def remove_bcs(mat, rhs, csr:bool=False):
+    # TODO : may do csr later..
+
+    N = mat.shape[0]
+    bcs = []
+    for i in range(N):
+        if mat[i,i] == 1.0:
+            bcs += [i]
+    
+    free_vars = np.array([_ for _ in range(N) if not(_ in bcs)])
+    mat = mat[free_vars,:][:,free_vars]
+    rhs = rhs[free_vars]
+    return mat, rhs
